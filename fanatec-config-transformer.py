c0397a99615b84f359f33e8c175a15f89e6c5ea9
John Evans <jevans@cedar.com>
2023-01-07 16:30:20 -0700

    Added base type and wheel type

--- fanatec-config-transformer.py
+++ fanatec-config-transformer.py
@@ -4,41 +4,41 @@ import xml.etree.ElementTree as ET
 import os
 import math
 
-path = "."
+path = "<path goes here>"
 dir_list = os.listdir(path)
 
 for folder in dir_list:
-    if os.path.isdir(folder):
-        subfolders = os.listdir(folder)
+    subfolders = os.listdir(path+"/"+folder)
 
-        for subfolder_file in subfolders:
-            if subfolder_file.endswith(".pws"):
-                
-                config_file = path+"/"+folder+"/"+subfolder_file
-                tree = ET.parse(config_file)
-                root = tree.getroot()
-                file_str = str(config_file)
-                tree = ET.parse(file_str)
-                root = tree.getroot()
+    for subfolder_file in subfolders:
+        if subfolder_file.endswith(".pws"):
+            
+            config_file = path+"/"+folder+"/"+subfolder_file
+            tree = ET.parse(config_file)
+            file_str = str(config_file)
+            tree = ET.parse(file_str)
+            root = tree.getroot()
 
-                #the following section takes FF value and multiplies by 1.25 then rounds up to a whole value
+            #the following section takes FF value and multiplies by 1.25 then rounds up to a whole value
 
-                for ffb in root.findall("TuningMenu"):
-                    ffb_value = ffb.find("FF")
+            for ffb in root.findall("TuningMenu"):
+                ffb_value = ffb.find("FF")
+                brf_value = ffb.find("BRF")
 
-                    ffb_value_str = ffb_value.text
-                    new_ffb_value = int(ffb_value_str) * 1.25
-                    ffb_dd1_value_int = math.ceil(new_ffb_value)
-                    ffb_dd1_value = str(ffb_dd1_value_int)
+                ffb_value_str = ffb_value.text
+                new_ffb_value = int(ffb_value_str) * 1.25
+                ffb_dd1_value_int = math.ceil(new_ffb_value)
+                ffb_dd1_value = str(ffb_dd1_value_int)
 
-                    ffb_value.text = ffb_dd1_value
+                ffb_value.text = ffb_dd1_value
+                brf_value.text = "95"
 
-                #here we can change the wheel type which I assume gets rid of the error message when loading on DD1
+            #here we can change the wheel type which I assume gets rid of the error message when loading on DD1
 
-                for wheel_type in root.findall("Device"):
-                    wheel_type_value = wheel_type.get("WheelType")
-                    wheel_type_value = 10
+            for wheel_type in root.findall("Device"):
+                wheel_type.set("WheelType", "14")
+                wheel_type.set("BaseType", "7")
 
-                tree.write(config_file)
+            tree.write(config_file)
 
 
